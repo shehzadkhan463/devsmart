226fac96a41ddde758b2053b255947dafb0ea740
shehzadkhan463 <shehzadkhan463@gmail.com>
2017-11-13 20:04:36 +0530

    First Commit 13-11-2017 modules fixed

--- src/app/front/front.module.ts
+++ src/app/front/front.module.ts
@@ -1,3 +1,4 @@
+import { FrontComponent } from './front.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
@@ -9,6 +10,7 @@ import {MatToolbarModule, MatMenuModule} from '@angular/material';
   imports: [
     CommonModule, MatToolbarModule, MatMenuModule
   ],
-  declarations: [LoginComponent, NavbarComponent, GridComponent]
+  declarations: [FrontComponent, LoginComponent, NavbarComponent, GridComponent],
+  exports: [FrontComponent]
 })
 export class FrontModule { }
